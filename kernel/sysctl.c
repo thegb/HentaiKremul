f75017d9269e6aad53af100c05157689e06170ee
Tyler Nijmeh <tylernij@gmail.com>
2019-03-06 18:17:07 -0800

    sched: Add separate config for exposing scheduler tunables
    Some desire to modify CFS tunables from userspace, but would not like
    the additional debug that comes with CONFIG_SCHED_DEBUG. The solution is
    to split up the configs: we now have CONFIG_SCHED_SYSCTL.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -274,7 +274,7 @@ static struct ctl_table sysctl_base_table[] = {
 	{ }
 };
 
-#ifdef CONFIG_SCHED_DEBUG
+#ifdef CONFIG_SCHED_SYSCTL
 static int min_sched_granularity_ns = 100000;		/* 100 usecs */
 static int max_sched_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_wakeup_granularity_ns;			/* 0 usecs */
@@ -283,7 +283,7 @@ static int max_wakeup_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #endif /* CONFIG_SMP */
-#endif /* CONFIG_SCHED_DEBUG */
+#endif /* CONFIG_SCHED_SYSCTL */
 
 #ifdef CONFIG_COMPACTION
 static int min_extfrag_threshold;
@@ -393,7 +393,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &two_million,
 	},
-#endif
 	{
 		.procname	= "sched_upmigrate",
 		.data		= &sysctl_sched_capacity_margin,
@@ -408,7 +407,8 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= sched_updown_migrate_handler,
 	},
-#ifdef CONFIG_SCHED_DEBUG
+#endif
+#ifdef CONFIG_SCHED_SYSCTL
 	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
@@ -541,7 +541,7 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &one,
 	},
 #endif /* CONFIG_NUMA_BALANCING */
-#endif /* CONFIG_SCHED_DEBUG */
+#endif /* CONFIG_SCHED_SYSCTL */
 	{
 		.procname	= "sched_rt_period_us",
 		.data		= &sysctl_sched_rt_period,
