bc9f11e0657181ed9370b8baec800f92f7d1b57f
Tanish <tanish2k09.dev@gmail.com>
2019-08-19 03:04:21 +0100

    driver: video: add klapse-5.0 and re-generate defcos
    tanish2k09/KLapse-Livedisplay@51b6d39e8a232591a3eb79a34e30776010e63b08
    
    Signed-off-by: Stephen Xian <3566565058@qq.com>
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -26,6 +26,9 @@
 #include "dsi_display.h"
 #include "dsi_ctrl_hw.h"
 
+#ifdef CONFIG_KLAPSE
+#include <linux/klapse.h>
+#endif
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 #include <asm/fcntl.h>
@@ -875,6 +878,10 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		pr_err("Backlight type(%d) not supported\n", bl->type);
 		rc = -ENOTSUPP;
 	}
+	
+#ifdef CONFIG_KLAPSE
+	set_rgb_slider(bl_lvl);
+#endif
 
 	if (((panel->last_bl_lvl == 0) || (panel->skip_dimmingon == STATE_DIM_RESTORE)) && bl_lvl) {
 		if (panel->panel_on_dimming_delay)
